df2ef5f09d0c19989c7fba45c325948cd65ab100
orozcoding <orozcoding@gmail.com>
2022-02-13 19:21:44 -0400

    create TodoContainer base component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class TodoContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello from Create React App</h1>
+        <p>I am in a React Component!</p>
+      </div>
+    );
+  }
+}
+
+export default TodoContainer;
