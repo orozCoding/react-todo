22ca4ab4d99acdee3e1f0f294f1d607f6e94c3ac
orozcoding <orozcoding@gmail.com>
2022-02-14 09:20:39 -0400

    create pages

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -3,6 +3,7 @@ import TodosList from './TodosList';
 import Header from './Header';
 import InputTodo from './InputTodo';
 import { v4 as uuidv4 } from "uuid";
+import { Route, Switch } from "react-router-dom";
 
 
 const TodoContainer = () => {
@@ -53,18 +54,6 @@ const TodoContainer = () => {
     )
   }
 
-  /*
-  useEffect(() => {
-    console.log("test run");
-    const temp = localStorage.getItem("todos")
-    const loadedTodos = JSON.parse(temp)
-
-    if(loadedTodos) {
-      setTodos(loadedTodos)
-    }
-  }, [setTodos])
-  */
-
   function getInitialTodos() {
     const temp = localStorage.getItem("todos")
     const savedTodos = JSON.parse(temp)
@@ -72,136 +61,24 @@ const TodoContainer = () => {
   }
 
   useEffect(() => {
-    // storing todos items
     const temp = JSON.stringify(todos)
     localStorage.setItem("todos", temp)
   }, [todos])
 
   return (
-    <div className="container">
-      <div className="inner">
-        <Header />
-        <InputTodo addTodoProps={addTodoItem} />
-        <TodosList
-          todos={todos}
-          handleChangeProps={handleChange}
-          deleteTodoProps={delTodo}
-          setUpdate={setUpdate}
-        />
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={addTodoItem} />
+          <TodosList
+            todos={todos}
+            handleChangeProps={handleChange}
+            deleteTodoProps={delTodo}
+            setUpdate={setUpdate}
+          />
+        </div>
       </div>
-    </div>
-  );
+  )
 }
-
-/*
-componentDidMount() {
-  const temp = localStorage.getItem("todos")
-  const loadedTodos = JSON.parse(temp)
-  if (loadedTodos) {
-    setTodos({
-      todos: loadedTodos
-    })
-  }
-}
-
-componentDidUpdate(prevProps, prevState) {
-  if(prevState.todos !== this.state.todos) {
-    const temp = JSON.stringify(this.state.todos)
-    localStorage.setItem("todos", temp)
-  }
-}
-*/
-
-
-/*
-class TodoContainer extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    todos: [],
-  };
-}
-
-handleChange = (id) => {
-  this.setState(prevState => ({
-    todos: prevState.todos.map(todo => {
-      if (todo.id === id) {
-        return {
-          ...todo,
-          completed: !todo.completed,
-        }
-      }
-      return todo
-    }),
-  }))
-};
-
-delTodo = (id) => {
-  this.setState({
-    todos: [
-      ...this.state.todos.filter(todo => {
-        return todo.id !== id;
-      })
-    ]
-  });
-};
-
-addTodoItem = (title) => {
-  const newTodo = {
-    id: uuidv4(),
-    title: title,
-    completed: false
-  };
-  this.setState({
-    todos: [...this.state.todos, newTodo]
-  })
-};
-
-setUpdate = (updatedTitle, id) => {
-  this.setState({
-    todos: this.state.todos.map(todo => {
-      if (todo.id === id) {
-        todo.title = updatedTitle
-      }
-      return todo
-    }),
-  })
-}
-
-componentDidMount() {
-  const temp = localStorage.getItem("todos")
-  const loadedTodos = JSON.parse(temp)
-  if (loadedTodos) {
-    this.setState({
-      todos: loadedTodos
-    })
-  }
-}
-
-componentDidUpdate(prevProps, prevState) {
-  if(prevState.todos !== this.state.todos) {
-    const temp = JSON.stringify(this.state.todos)
-    localStorage.setItem("todos", temp)
-  }
-}
-
-render() {
-  return (
-    <div className="container">
-      <div className="inner">
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-          setUpdate={this.setUpdate}
-        />
-      </div>
-    </div>
-  );
-}
-}
-*/
 
 export default TodoContainer;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoContainer from './functionBased/components/TodoContainer';
-import "./classBased/App.css"
+import "./classBased/App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
